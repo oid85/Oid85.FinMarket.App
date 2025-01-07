acf503b381595bded2ded9e68c0448679919275a
oid85 <oid85@mail.ru>
2025-01-07 22:59:45 +0300

    страницы для акций

---
+++ oid85-finmarket-app/src/component/Screens/Shares/ReportSharesAssetFundamental.js
@@ -0,0 +1,44 @@
+import React, { useEffect } from 'react'
+import {useDispatch, useSelector} from 'react-redux'
+import Loader from '../../Loader/Loader'
+import { sagaReportSharesAssetFundamental } from '../../../redux/actions/reportSharesActions'
+import '../../Report/Report.css'
+import { Table } from '../../Report/Table'
+import {FilterByLastDaysGroup} from '../../Filter/FilterByLastDaysGroup'
+
+export const ReportSharesAssetFundamental = () => {
+
+    const dispatch = useDispatch()
+    const loading = useSelector(state => state.app.loading)
+    const reportData = useSelector(state => state.reportShares.reportData)
+    const startDate = useSelector(state => state.filter.startDate)
+    const endDate = useSelector(state => state.filter.endDate)
+
+    useEffect(() => {
+        dispatch(sagaReportSharesAssetFundamental())
+    }, [])
+
+    useEffect(() => {
+        dispatch(sagaReportSharesAssetFundamental())
+    }, [startDate])
+
+    useEffect(() => {
+        dispatch(sagaReportSharesAssetFundamental())
+    }, [endDate])
+
+    return (
+        <React.Fragment>
+            {
+                !reportData.result || loading
+                    ? <Loader/>
+                    :                    
+                    <div className='report-container'>
+                        <FilterByLastDaysGroup />
+                        <Table
+                            title = {`${reportData.result.title}`}
+                            reportTableData = {reportData.result} />
+                    </div>                    
+            }
+        </React.Fragment>
+    )
+}
--- oid85-finmarket-app/src/component/Screens/Shares/SharesScreen.js
+++ oid85-finmarket-app/src/component/Screens/Shares/SharesScreen.js
@@ -6,6 +6,10 @@ import { Alert } from '../../Alert/Alert'
 import {ReportSharesSupertrend} from "./ReportSharesSupertrend";
 import {ReportSharesCandleSequence} from "./ReportSharesCandleSequence";
 import {ReportSharesCandleVolume} from "./ReportSharesCandleVolume";
+import {ReportSharesRsi} from "./ReportSharesRsi";
+import {ReportSharesYieldLtm} from "./ReportSharesYieldLtm";
+import {ReportSharesDividend} from "./ReportSharesDividend";
+import {ReportSharesAssetFundamental} from "./ReportSharesAssetFundamental";
 
 export const SharesScreen = () => {
     const alert = useSelector(state => state.app.alert)
@@ -18,6 +22,10 @@ export const SharesScreen = () => {
                     <Tab>Supertrend</Tab>
                     <Tab>CandleSequence</Tab>
                     <Tab>CandleVolume</Tab>
+					<Tab>Rsi</Tab>
+					<Tab>YieldLtm</Tab>
+					<Tab>Dividend</Tab>
+					<Tab>AssetFundamental</Tab>
                 </TabList>
                 <TabPanel>
                     <ReportSharesSupertrend />
@@ -28,6 +36,18 @@ export const SharesScreen = () => {
                 <TabPanel>
                     <ReportSharesCandleVolume />
                 </TabPanel>
+                <TabPanel>
+                    <ReportSharesRsi />
+                </TabPanel>
+                <TabPanel>
+                    <ReportSharesYieldLtm />
+                </TabPanel>
+                <TabPanel>
+                    <ReportSharesDividend />
+                </TabPanel>
+                <TabPanel>
+                    <ReportSharesAssetFundamental />
+                </TabPanel>				
             </Tabs>
         </React.Fragment>
     )    
