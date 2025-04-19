90e4bfd992c49425dfa5fdce3bfd6b2132b5e252
oid85 <oid85@mail.ru>
2025-04-19 21:11:00 +0700

    Update FilterBySharesTickerListGroup.js

--- oid85-finmarket-app/src/component/Filter/FilterByTickerList/FilterBySharesTickerListGroup.js
+++ oid85-finmarket-app/src/component/Filter/FilterByTickerList/FilterBySharesTickerListGroup.js
@@ -7,12 +7,9 @@ export const FilterBySharesTickerListGroup = () => {
     let dict = {};
 
     dict['Лист. набл.'] = 'shares/watchlist'
-    dict['Эшелон 1'] = 'shares/echelons_mcap/echelon_1_blue_chips'
-    dict['Эшелон 2'] = 'shares/echelons_mcap/echelon_2_middle_mcap'
-    dict['Эшелон 3'] = 'shares/echelons_mcap/echelon_3_little_mcap'
-    dict['Эшелон 4'] = 'shares/echelons_mcap/echelon_4_mini_mcap'
-    dict['Эшелон 5'] = 'shares/echelons_mcap/echelon_5_micro_mcap'
-    dict['Эшелон 6'] = 'shares/echelons_mcap/echelon_6_nano_mcap'
+    dict['Эшелон 1'] = 'shares/echelons/echelon_1'
+    dict['Эшелон 2'] = 'shares/echelons/echelon_2'
+    dict['Эшелон 3'] = 'shares/echelons/echelon_3'
     dict['Банки'] = 'shares/sectors/banks'
     dict['Эл. сети'] = 'shares/sectors/electricity_networks'
     dict['Эн. ген.'] = 'shares/sectors/energy_generation'
@@ -35,9 +32,6 @@ export const FilterBySharesTickerListGroup = () => {
         'Эшелон 1',
         'Эшелон 2',
         'Эшелон 3',
-        'Эшелон 4',
-        'Эшелон 5',
-        'Эшелон 6',
         'Банки',
         'Эл. сети',
         'Эн. ген.',
