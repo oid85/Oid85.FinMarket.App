cf91bf0f51e4dabd0073088d9e83721a952eaf55
oid85 <oid85@mail.ru>
2025-07-17 19:13:03 +0700

    Update DiagramSharesMultiplicatorsMcapPeNetDebtEbitda.js

--- oid85-finmarket-app/src/component/Screens/Shares/DiagramSharesMultiplicatorsMcapPeNetDebtEbitda.js
+++ oid85-finmarket-app/src/component/Screens/Shares/DiagramSharesMultiplicatorsMcapPeNetDebtEbitda.js
@@ -1,37 +1,10 @@
 import React, { useEffect } from 'react'
-import {useDispatch, useSelector} from 'react-redux'
-import Loader from '../../Loader/Loader'
-import { sagaDiagramSharesMultiplicatorsMcapPeNetDebtEbitda } from '../../../redux/actions/diagramSharesActions'
 import '../../Diagram/Diagram.css'
-import {BubbleDiagram} from "../../Diagram/BubbleDiagram"
-import {FilterBySharesTickerListGroup} from "../../Filter/FilterByTickerList/FilterBySharesTickerListGroup";
 
 export const DiagramSharesMultiplicatorsMcapPeNetDebtEbitda = () => {
 
-    const dispatch = useDispatch()
-    const loading = useSelector(state => state.app.loading)
-    const diagramData = useSelector(state => state.diagramShares.diagramData)
-    const tickerList = useSelector(state => state.filter.sharesTickerList)
-
-    useEffect(() => {
-        dispatch(sagaDiagramSharesMultiplicatorsMcapPeNetDebtEbitda())
-    }, [])
-
-    useEffect(() => {
-        dispatch(sagaDiagramSharesMultiplicatorsMcapPeNetDebtEbitda())
-    }, [tickerList])
-
     return (
         <React.Fragment>
-            {
-                !diagramData.result || loading
-                    ? <Loader/>
-                    :                    
-                    <div className='diagram-container'>
-                        <FilterBySharesTickerListGroup />
-                        <BubbleDiagram data = {diagramData.result} />
-                    </div>                    
-            }
         </React.Fragment>
     )
 }
