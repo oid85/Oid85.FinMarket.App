5cbc97821d9521f59c245b1cd74e39991b692c08
oid85 <oid85@mail.ru>
2025-07-24 23:13:31 +0700

    ,

--- oid85-finmarket-app/src/component/Diagram/SpreadDiagram.js
+++ oid85-finmarket-app/src/component/Diagram/SpreadDiagram.js
@@ -1,5 +1,5 @@
 ﻿import React from 'react'
-import { AreaChart, Area, YAxis, CartesianGrid, Tooltip } from 'recharts'
+import {BarChart, Area, YAxis, CartesianGrid, Tooltip, Bar} from 'recharts'
 import {Attention} from "../Icon/Attention"
 
 export const SpreadDiagram = ({ key, title, series }) => {
@@ -7,12 +7,12 @@ export const SpreadDiagram = ({ key, title, series }) => {
         <div>
             <Attention visible={false}/>
             <div>{title}</div>
-            <AreaChart key = {key} width={600} height={250} data={series}>
+            <BarChart key = {key} width={600} height={250} data={series}>
                 <CartesianGrid strokeDasharray="3 3" />
                 <YAxis type="number" domain={['auto', 'auto']} />
                 <Tooltip />
-                <Area type="monotone" dataKey="value" stroke="#8884d8" fill="#8884d8" />
-            </AreaChart>
+                <Bar type="monotone" dataKey="value" stroke="#8884d8" fill="#8884d8" />
+            </BarChart>
         </div>
     )
 }
