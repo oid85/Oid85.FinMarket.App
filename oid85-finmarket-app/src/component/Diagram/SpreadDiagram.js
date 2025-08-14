37b8260fd0c0769e0534f67613dd40e0b985ffbd
oid85 <oid85@mail.ru>
2025-08-14 20:47:57 +0700

    .

--- oid85-finmarket-app/src/component/Diagram/SpreadDiagram.js
+++ oid85-finmarket-app/src/component/Diagram/SpreadDiagram.js
@@ -6,19 +6,20 @@ export const SpreadDiagram = ({ key, title, series }) => {
     return (
         <div
             style={{
-                border: '2px solid gray'
+                border: '1px solid black',
+                margin: '10px',
             }}>
             <h6>{title}</h6>
             <BarChart key = {key} width={450} height={150} data={series}>
                 <YAxis type="number" domain={[-3, 3]} />
                 <Tooltip />
-                <ReferenceLine y={1} stroke={CONSTANTS.COLOR_GRAY} />
-                <ReferenceLine y={2} stroke={CONSTANTS.COLOR_GRAY} />
-                <ReferenceLine y={3} stroke={CONSTANTS.COLOR_GRAY} />
-                <ReferenceLine y={-1} stroke={CONSTANTS.COLOR_GRAY} />
-                <ReferenceLine y={-2} stroke={CONSTANTS.COLOR_GRAY} />
-                <ReferenceLine y={-3} stroke={CONSTANTS.COLOR_GRAY} />
-                <Bar type="monotone" dataKey="value" stroke={CONSTANTS.COLOR_DARKGREEN} fill={CONSTANTS.COLOR_DARKGREEN} />
+                <ReferenceLine y={1} stroke={CONSTANTS.COLOR_BLACK} />
+                <ReferenceLine y={2} stroke={CONSTANTS.COLOR_BLACK} />
+                <ReferenceLine y={3} stroke={CONSTANTS.COLOR_BLACK} />
+                <ReferenceLine y={-1} stroke={CONSTANTS.COLOR_BLACK} />
+                <ReferenceLine y={-2} stroke={CONSTANTS.COLOR_BLACK} />
+                <ReferenceLine y={-3} stroke={CONSTANTS.COLOR_BLACK} />
+                <Bar type="monotone" dataKey="value" stroke={CONSTANTS.COLOR_BLACK} fill={CONSTANTS.COLOR_GREEN} />
             </BarChart>
         </div>
     )
